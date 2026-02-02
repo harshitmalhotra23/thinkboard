6448c04ba0e3cb20e2f0f5995a06cad90bc12dc4
Harshit <harshitmalhotra28@gmail.com>
2026-02-02 22:34:10 +0530

    Completed homepage and initialized frontend

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -1,12 +1,12 @@
-import {Ratelimit} from "@upstash/ratelimit"
-import {Redis} from "@upstash/redis"
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100, "40 s"),
-})
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "30 s"),
+});
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,27 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
 
 const app = express();
 
-
-
 //middleware to parse JSON bodies
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
 
-
-connectDB().then(()=>{
-    app.listen(process.env.PORT , ()=>{
-        console.log(`Server is running on port ${process.env.PORT}`);
-    })
-})
-
-
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
