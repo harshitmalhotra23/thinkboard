2484a3ef83305b568493a466e1fa9df5a7446853
Harshit <harshitmalhotra28@gmail.com>
2026-02-05 04:02:57 +0530

    Minor changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,37 @@ import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/ratelimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 //middleware to parse JSON bodies
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.use((req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+  }
+}
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
